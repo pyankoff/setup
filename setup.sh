602b76f7c79cff0f6d02fc360a972d1eeaa2033e
Andrey Pyankov <pyankoff@users.noreply.github.com>
2016-03-17 13:12:42 +0300

    dotfiles fork

--- setup.sh
+++ setup.sh
@@ -65,7 +65,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/pyankoff/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
