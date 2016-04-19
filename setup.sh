d5886f3779362e9872941b5cb4a66eaebc13df47
Andrey Pyankov <pyankoff@users.noreply.github.com>
2016-04-20 01:04:53 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -89,5 +89,6 @@ ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
 # set path to clackup
-export PATH=$PATH:~/.roswell/bin
-echo "alias sudo='sudo env PATH=$PATH'" >> .bashrc_cusom
+echo "export PATH=$PATH:~/.roswell/bin" >> .bashrc_custom
+echo "alias sudo='sudo env PATH=$PATH'" >> .bashrc_custom
+source .bashrc
