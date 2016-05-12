6f44391f1dbe91d376f7f49e05db35dd81cb0890
Andrey Pyankov <pyankoff@users.noreply.github.com>
2016-05-12 11:55:03 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -66,7 +66,7 @@ export PATH=$PATH:/home/ubuntu/.roswell/bin
 sudo apt-get install -y libserver-starter-perl
 
 # Anaconda
-curl https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-4.0.0-Linux-x86_64.sh | sh
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-4.0.0-Linux-x86_64.sh | sh
 
 source ~/.bashrc
 
