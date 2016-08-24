2a5139173f011d677a1a8000880250f8552a7135
Andrey Pyankov <pyankoff@users.noreply.github.com>
2016-08-24 18:50:03 +0300

    new npm

--- setup.sh
+++ setup.sh
@@ -4,34 +4,20 @@
 
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
+sudo apt-get update -y
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-sudo apt-get install -y npm
-sudo apt-get install -y node
-npm install -g jshint
-
-# Install rlwrap to provide libreadline features with node
-# See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
+sudo npm install -g npm
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
 sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
-
-# Install Heroku toolbelt
-# https://toolbelt.heroku.com/debian
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # Make
 sudo apt-get install -y gcc
